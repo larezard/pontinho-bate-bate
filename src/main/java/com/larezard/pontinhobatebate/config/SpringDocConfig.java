06971e39d5c306afce9d988f0d97b3e3e711bb0b
Ricardo Antonio de Souza <ricardo.souza@clickbus.com>
2022-07-15 12:55:24 -0400

    Swagger configuration changed

--- src/main/java/com/larezard/pontinhobatebate/config/SpringDocConfig.java
+++ src/main/java/com/larezard/pontinhobatebate/config/SpringDocConfig.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenApiConfig {
+public class SpringDocConfig {
 
   @Bean
-  public OpenAPI pontinhoBateBateOpenAPI() {
+  public OpenAPI pontinhoBateBateAPI() {
     return new OpenAPI()
             .info(new Info()
                     .title("Pontinho Bate Bate API")
