0a40b0cb8b9b899336c268ebd5ed43abe11cd12c
larissa <rocha.dbx@gmail.com>
2022-07-15 13:03:26 -0300

    initial commit

---
+++ src/main/java/com/larezard/pontinhobatebate/PontinhoBateBateApplication.java
@@ -0,0 +1,13 @@
+package com.larezard.pontinhobatebate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontinhoBateBateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontinhoBateBateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larezard/pontinhobatebate/PontinhoBateBateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larezard.pontinhobatebate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontinhoBateBateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
